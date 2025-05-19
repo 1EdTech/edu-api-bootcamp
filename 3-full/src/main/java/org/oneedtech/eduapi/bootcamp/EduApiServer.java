ceeca9218634e1793e637249dea01228b36f8798
Xavi Aracil <xaracil@1edtech.org>
2025-05-19 16:23:43 +0200

    Copied from 2- openapi

---
+++ 3-full/src/main/java/org/oneedtech/eduapi/bootcamp/EduApiServer.java
@@ -0,0 +1,12 @@
+package org.oneedtech.eduapi.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduApiServer {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EduApiServer.class, args);
+  }
+}
