637dbf777374c2aeea98c99d1fd0939ab2c8ce57
Xavi Aracil <xaracil@1edtech.org>
2025-05-19 16:20:13 +0200

    Forced order so it can be processed first

--- 1-initial/src/main/java/org/oneedtech/eduapi/bootcamp/ResourceServerSecurityConfig.java
+++ 1-initial/src/main/java/org/oneedtech/eduapi/bootcamp/ResourceServerSecurityConfig.java
@@ -4,6 +4,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,6 +23,7 @@ public class ResourceServerSecurityConfig {
      * @throws Exception
      */
     @Bean
+    @Order(1)
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.securityMatcher("/hello/**")
           .authorizeHttpRequests(authorize -> authorize.anyRequest()
